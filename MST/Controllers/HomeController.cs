a57fdf8a13afb640e3053445657b37142055952d
Tan Sophary <Sophary.Tan@icckh.com>
2025-10-15 15:41:53 +0700

    add service and admin page

--- MST/Controllers/HomeController.cs
+++ MST/Controllers/HomeController.cs
@@ -33,6 +33,10 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Service()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
